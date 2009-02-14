da99f56c1ffa0c1fd11828abb639612603ec0ca5
Jonathan Rockway <jon@jrock.us>
2009-02-14 02:00:29 -0600

    blah

--- echo-server.pl
+++ echo-server.pl
@@ -17,41 +17,51 @@ my $s = tcp_server undef, 1234, sub {
         fh => $fh,
     );
 
+    my $buf = "";
     my $reader;
-    $handle->push_read( json => \&reader );
+    $reader = sub {
+        my $handle = shift;
+        my $obj = shift;
+        my ($type, $value) = map { $obj->{$_} } qw/type value/;
+
+        { no warnings 'uninitialized';
+          say "Got message.  Type: $type, value '$value'";
+        }
+
+        given($type){
+            when('exit'){
+                $exit->send(0);
+            }
+            when('keyPress') {
+                my $char = chr $value;
+                given($char){
+                    when("\n"){
+                        $handle->push_write( json => {
+                            type  => 'normalOutput',
+                            value => "$buf\n",
+                        });
+                        $handle->push_write( "\r\n" );
+                        $buf = "";
+                    }
+                    default {
+                        $buf .= chr $value;
+                    }
+                }
+
+                $handle->push_read( json => $reader );
+            }
+            default {
+                $handle->push_write( json => {
+                    type  => 'errorOutput',
+                    value => 'unknown command',
+                });
+                $handle->push_write( "\r\n" );
+                $handle->push_read( json => $reader );
+            }
+        }
+    };
+
+    $handle->push_read( json => $reader );
 };
 
 exit $exit->recv;
-
-sub reader {
-    my $handle = shift;
-    my $obj = shift;
-    my ($type, $value) = map { $obj->{$_} } qw/type value/;
-
-    { no warnings 'uninitialized';
-      say "Got message.  Type: $type, value '$value'";
-    }
-
-    given($type){
-        when('exit'){
-            $exit->send(0);
-        }
-        when('keyPress') {
-            $handle->push_write( json => {
-                type  => 'normalOutput',
-                value => chr $value,
-            });
-            $handle->push_write( "\r\n" );
-            $handle->push_read( json => \&reader );
-        }
-        default {
-            $handle->push_write( json => {
-                type  => 'errorOutput',
-                value => 'unknown command',
-            });
-            $handle->push_write( "\r\n" );
-            $handle->push_read( json => \&reader );
-        }
-    }
-}
-
